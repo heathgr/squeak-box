b29597db1a95c47e3db7bb2fbd6a3b9125fbbfbf
Grant Heath <heathgr@gmail.com>
2020-01-26 12:14:11 -0700

    Render first react component.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,7 @@
-console.log('It works!!!!!')
+import { createElement as e } from 'react'
+import { render } from 'react-dom'
+
+const appContainer = document.getElementById('app')
+const App = () => e('h1', null, 'Squeak Box')
+
+render(e(App), appContainer)
