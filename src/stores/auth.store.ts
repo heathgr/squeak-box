65b54d07116b48a0269f34ec22beee3142b00950
Grant Heath <heathgr@gmail.com>
2020-02-01 12:36:38 -0700

    Rename AuthStore interface to AuthState.

--- src/stores/auth.store.ts
+++ src/stores/auth.store.ts
@@ -1,14 +1,14 @@
 import { User } from 'firebase'
 import { createStore } from 's-is-for-store'
 
-export interface AuthStore {
+export interface AuthState {
   user: User | null,
 }
 
-export const initialState: AuthStore = {
+export const initialState: AuthState = {
   user: null,
 }
 
-const authStore = createStore<AuthStore>(initialState)
+const authStore = createStore<AuthState>(initialState)
 
 export default authStore
