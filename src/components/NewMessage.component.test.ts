da9f48472d7a0a3f6224f2e2f6d9bccd5f16a314
Grant Heath <heathgr@gmail.com>
2020-02-02 09:43:39 -0700

    NewMessage component now creates a new message when enter is pressed.

--- src/components/NewMessage.component.test.ts
+++ src/components/NewMessage.component.test.ts
@@ -1,20 +1,59 @@
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
 import { createElement as e } from 'react'
 
 import NewMessage from './NewMessage.component'
+import * as messageUpdater from '../updaters/messages.updater'
 
 describe('New Message Component', () => {
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
   it('Has a text input field that limits input to 80 characters.', () => {
-    const subject = shallow(e(NewMessage))
+    const subject = mount(e(NewMessage))
     const testId = '[data-test-id="message-input"]'
     const textInput = subject.find(testId)
 
     expect(textInput.exists()).toEqual(true)
 
-    const testValue = 'Just a test'
-    textInput.simulate('change', { target: { value: testValue } })
+    const longInput = 'This is a very long test message.  It has more than 80 characters in it.  It is not a valid message!!!'
+    const expected = longInput.substring(0, 80)
+    textInput.simulate('change', { target: { value: longInput } })
 
-    subject.update()
-    expect(subject.find(testId).prop('value')).toEqual(testValue)
+    expect(subject.find(testId).prop('value')).toEqual(expected)
+  })
+
+  it('Has a button that invokes createMessage when clicked.', () => {
+    const subject = mount(e(NewMessage))
+    const buttonTestId = '[data-test-id="submit-button"]'
+    const inputTestId = '[data-test-id="message-input"]'
+    const textInput = subject.find(inputTestId)
+    const submitButton = subject.find(buttonTestId)
+    const createMessageSpy = jest
+      .spyOn(messageUpdater, 'createMessage')
+      .mockImplementation((): void => undefined)
+    const testMessage = 'This is a test message.'
+
+    textInput.simulate('change', { target: { value: testMessage } })
+    submitButton.simulate('click')
+
+    expect(createMessageSpy).toHaveBeenCalledTimes(1)
+    expect(createMessageSpy).toHaveBeenCalledWith(testMessage)
+  })
+
+  it('Has an input field that invokes createMessage when enter is pressed.', () => {
+    const subject = mount(e(NewMessage))
+    const inputTestId = '[data-test-id="message-input"]'
+    const textInput = subject.find(inputTestId)
+    const createMessageSpy = jest
+      .spyOn(messageUpdater, 'createMessage')
+      .mockImplementation((): void => undefined)
+    const testMessage = 'This is a test message.'
+
+    textInput.simulate('change', { target: { value: testMessage } })
+    textInput.simulate('keyDown', { key: 'Enter' })
+
+    expect(createMessageSpy).toHaveBeenCalledTimes(1)
+    expect(createMessageSpy).toHaveBeenCalledWith(testMessage)
   })
 })
--- src/components/NewMessage.component.ts
+++ src/components/NewMessage.component.ts
@@ -3,6 +3,9 @@ import { createMessage } from '../updaters/messages.updater'
 
 const NewMessage: FC = () => {
   const [inputValue, setInputValue] = useState('')
+  const handleMessageCreation = (): void => {
+    createMessage(inputValue)
+  }
 
   return e(
     'div',
@@ -14,6 +17,9 @@ const NewMessage: FC = () => {
         onChange: (evt) => {
           setInputValue(evt.target.value.substring(0, 80))
         },
+        onKeyDown: (evt) => {
+          if (evt.key === 'Enter') handleMessageCreation()
+        },
         type: 'text',
         value: inputValue,
       },
@@ -22,9 +28,8 @@ const NewMessage: FC = () => {
     e(
       'button',
       {
-        onClick: () => {
-          createMessage(inputValue)
-        },
+        'data-test-id': 'submit-button',
+        onClick: handleMessageCreation,
       },
       'Send',
     ),
