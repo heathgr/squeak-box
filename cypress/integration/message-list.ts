78d8c9baf8d2c4b5a21cd505054762040744bc70
Grant Heath <heathgr@gmail.com>
2020-03-24 21:30:12 -0600

    Improved e2e test.

--- cypress/integration/message-list.ts
+++ cypress/integration/message-list.ts
@@ -1,7 +1,13 @@
 
-it('loads examples', () => {
+it('Displays messages.', () => {
   cy.clearDatabase()
-  cy.addMessage('junkId1', 'Kitchen Sink', new Date())
+  cy.addMessage('testId1', 'Hello there!', new Date(2020, 1, 1, 3))
+  cy.addMessage('testId2', 'I like turtles!', new Date(2020, 1, 1, 2))
+  cy.addMessage('testId3', ':)', new Date(2020, 1, 1, 1))
   cy.visit('./public/index.html')
-  cy.contains('Kitchen Sink')
+
+  cy.get('[data-test-id="message"]').should('have.length', 3)
+  cy.get('[data-test-id="message"]').eq(0).should('have.text', 'Hello there!')
+  cy.get('[data-test-id="message"]').eq(1).should('have.text', 'I like turtles!')
+  cy.get('[data-test-id="message"]').eq(2).should('have.text', ':)')
 })
--- src/components/Message.component.ts
+++ src/components/Message.component.ts
@@ -1,6 +1,12 @@
 import { createElement as e, FC } from 'react'
 import { MessageState } from '../stores/messages.store'
 
-const Message: FC<MessageState> = (props) => e('div', null, props.message)
+const Message: FC<MessageState> = (props) => e(
+  'div',
+  {
+    'data-test-id': 'message',
+  },
+  props.message,
+)
 
 export default Message
