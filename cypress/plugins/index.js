2d470a9e9095bbfbb99ed7921b623fddb805ca2d
Grant Heath <heathgr@gmail.com>
2020-03-24 20:40:07 -0600

    Reset client API key.

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -6,13 +6,9 @@ module.exports = (on) => {
   on('file:preprocessor', cypressTypeScriptPreprocessor)
   on('task', {
     clearDatabase() {
-      console.log('Fuck: ', deleteAll)
-      const deletePromise = deleteAll()
-      console.log('Shit: ', typeof deletePromise)
       return deleteAll()
     },
     addMessage(document) {
-      console.log('Adding message!!!: ', document)
       return setDocument(`/messages/${document.id}`, {
         message: document.message,
         createdAt: document.createdAt,
