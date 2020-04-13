08dfb0adfd1bfee7e398e17cdcafc7cd3fe01cea
Grant Heath <heathgr@gmail.com>
2020-04-12 19:59:41 -0600

    Initial firestore function test.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
-// import * as functions from 'firebase-functions';
+/* eslint-disable import/prefer-default-export */
+import * as functions from 'firebase-functions';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +7,9 @@
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+export const justATest = functions.firestore.document('/messages/{message}').onCreate((change, context) => {
+  console.log('data: ', change.data())
+  console.log('auth: ', context.auth)
+  return true
+})
